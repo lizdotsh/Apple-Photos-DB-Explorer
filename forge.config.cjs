db8cc2c4d52f4429b76a6877c16aacec2e13352e
liz <lizjunkbox@gmail.com>
2023-10-17 09:50:08 -0700

    build

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,25 +1,46 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    ignore: [
+        /^\/src/,
+        /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
+        /^\/node_modules\/.vite/,
+      ],
+    extendInfo: {
+      NSPhotoLibraryUsageDescription:
+        "This program needs access to your photos library db",
+      osxSign: {
+        identity: 'liz inerati',
+      },
+      osxNotarize: false, 
+    },
+  },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-      config:{
-        arch: ["arm64", "x64"]
-      },
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
+    },
+    {
+      name: "@electron-forge/maker-dmg",
     },
   ],
+//   plugins: [
+//     {
+//       name: '@electron-forge/plugin-vite',
+      
+    
+//     }
+    // ],
 };
