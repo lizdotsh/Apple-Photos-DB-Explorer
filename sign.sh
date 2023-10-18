5175458537c97605c8eb0c0cd887199c1a9860ce
liz <lizjunkbox@gmail.com>
2023-10-18 13:06:39 -0700

    signing script thing(WIP)

---
+++ sign.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+wk=$(pwd)
+
+# Sign the app
+output_dir="out/make/zip/darwin/arm64/signed"
+mkdir -p "$output_dir/unzip"
+
+
+while getopts "o:" opt; do
+  case $opt in
+    o)
+      output_dir="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+# Get name of file to sign
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+filename=$(basename "$1")
+
+# make out/make/zip/darwin/arm64 folder if not exists
+# 1. Unzip the file    
+unzip -o "$1" -d "$output_dir/unzip" 
+
+cd "$output_dir"
+# 2. Sign the file
+
+codesign --force --deep -s 'liz inerati' "unzip/apple-photos-db-explorer.app"
+
+
+# 3. Zip the file
+#  zip -r "unzip/$filename" "$output_dir/unzip/apple-photos-db-explorer.app"
+(cd "unzip/" && zip -r - .) > "$filename"
+cd $wk
