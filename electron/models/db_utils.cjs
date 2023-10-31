c6908065e1c8a8d39406832d1b8e5a0d881c5fa3
liz <87026097+lizdotsh@users.noreply.github.com>
2023-10-31 15:35:11 -0700

    changed db loading to be sync and use db_utils.js (#14)

--- electron/models/db_utils.cjs
+++ electron/models/db_utils.cjs
@@ -2,7 +2,7 @@ const sqlite = require("better-sqlite3");
 const path = require("path");
 const { app } = require("electron");
 const db_path = path.join(app.getPath("userData"), "photo_lib.sqlite");
-const db = new sqlite(db_path);
+let db = new sqlite(db_path);
 exports.db = db;
 exports.txGetAll = function (query, params) {
   let result;
@@ -11,6 +11,20 @@ exports.txGetAll = function (query, params) {
   })();
   return result;
 };
+exports.resetDb = function () {
+    db.close();
+    db = new sqlite(db_path);
+    exports.db = db;
+
+}
+exports.txExecAll = function (file) {
+    db.transaction(() => {
+        db.exec(file);
+    })();
+};
+exports.getPragma = function (query) {
+    return db.pragma(query);
+}
 exports.txGetOne = function (query, params) {
   let result;
   db.transaction(() => {
